e8beb7b54d69224cfd8299ddf7b30014d67219c3
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2025-05-21 16:45:34 -0300

    Deleting Hello World example

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+  app.enableCors({
+    origin: frontendUrl,
+  });
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`App corriendo en http://localhost:${port}`);
+  console.log(`Aceptando CORS desde: ${frontendUrl}`);
 }
 bootstrap();
